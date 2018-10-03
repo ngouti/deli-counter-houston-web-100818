8eeb727ce10c56bd1d3ea4ea8e4bbc89865f7eae
Emma Gouti <goutiemma@gmail.com>
2018-10-03 05:31:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+# Write your code here
+katz_deli = [];
+
+def line(array)
+  current = [];
+  if array.length == 0
+    puts "The line is currently empty."
+else 
+  array.each do |index, name|
+    current.push("#{index}. #{name}")
+  end
+ puts "The line is currently: #{current.join(" ")}"
+end
+end
+def take_a_number(array, name)
+end
+  
