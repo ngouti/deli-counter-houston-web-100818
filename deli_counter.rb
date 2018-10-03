25b6d0849085b8d7ab302a5d8063441b480857c4
Emma Gouti <goutiemma@gmail.com>
2018-10-03 05:40:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,6 @@ end
 
 def take_a_number(array, name)
   array << name;
-  puts Welcome, #{name}. You are number #{array.length} in line."
-end
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
   
