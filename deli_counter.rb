d13c198dcc2a27cdf14f7759d7e128434f5ce03a
Emma Gouti <goutiemma@gmail.com>
2018-10-03 05:33:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,13 @@ katz_deli = [];
 
 def line(array)
   current = [];
-  counter = 0;
+  counter = 1;
   if array.length == 0
     puts "The line is currently empty."
 else 
   array.each do |name|
-    current.push("#{counter+1}. #{name} ")
+    current.push("#{counter}. #{name} ")
+    counter += 1;
   end
  puts "The line is currently: #{current.join(" ")}"
 end
