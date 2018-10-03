69ef2e8dffc993b2eb8889fb5a51052a59f05054
Emma Gouti <goutiemma@gmail.com>
2018-10-03 05:44:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,10 +16,15 @@ end
 end
 
 
-
-
 def take_a_number(array, name)
   array << name;
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
   
+  def now_serving(array)
+    if array.length == 0 
+      puts "The line is currently empty."
+    else
+      puts "Now swering: #{array.shift()}
+    
+  end
